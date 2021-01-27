99a7fac6aa3fea89bf071a97950bb3df4385e835
atharva <atharva55@yahoo.com>
2021-01-27 23:21:45 +0530

    make model UserLoginHistory for project

--- Myproject/Users/models.py
+++ Myproject/Users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.dispatch import receiver
+
+class UserLoginHistory(models.Model):
+    ip = models.GenericIPAddressField(null=True)
+    username = models.CharField(max_length=256, null=True)
+
+    def __str__(self):
+        return self.ip
+
+@receiver(user_logged_in)
+def user_logged_in_callback(sender, request, user, **kwargs):  
+    ip = request.META.get('REMOTE_ADDR')
+    UserLoginHistory.objects.create(ip=ip, username=user.username)
+
