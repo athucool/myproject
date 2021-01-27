2a0fe5218b400c4451dc98abb75052c8759ba6aa
atharva <atharva55@yahoo.com>
2021-01-27 23:20:51 +0530

    update admin.py register UserLoginHistory in admin.py

--- Myproject/Users/admin.py
+++ Myproject/Users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+# from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import UserLoginHistory
+ 
 
-# Register your models here.
+@admin.register(UserLoginHistory)
+class Useradmin(ImportExportModelAdmin):
+    list_display=("username","ip")
+
